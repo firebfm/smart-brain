968e3bf753bd9316dd34e8306ca3f4e3fc9eee71
firebfm <firebfm@gmail.com>
2020-09-15 00:57:42 -0400

    heroku connect

--- src/App.js
+++ src/App.js
@@ -57,11 +57,11 @@ class App extends Component {
     }})
   }
 
-  componentDidMount() {
-    fetch('http://localhost:3000')
-      .then(response => response.json())
-      .then(console.log)
-  }
+  // componentDidMount() {
+  //   fetch('http://localhost:3000/')
+  //     .then(response => response.json())
+  //     .then(console.log)
+  // }
 
   calculateFaceLocation = (data) => {
     const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
@@ -88,7 +88,7 @@ class App extends Component {
     this.setState({imageUrl: this.state.input});
     app.models.predict(Clarifai.FACE_DETECT_MODEL, this.state.input).then(response => {
       if (response) {
-        fetch('http://localhost:3000/image' , {
+        fetch('https://warm-reaches-15453.herokuapp.com/image' , {
           method: 'put',
           headers: {'Content-Type': 'application/json'},
           body: JSON.stringify({
