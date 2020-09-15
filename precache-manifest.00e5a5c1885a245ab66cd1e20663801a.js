46cbd2c7b0a6031fc83d13ed4791726f91da0e20
firebfm <firebfm@gmail.com>
2020-09-15 01:39:11 -0400

    Updates

---
+++ precache-manifest.00e5a5c1885a245ab66cd1e20663801a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aef7c24a04d656ee9cb8c49e48a396ef",
+    "url": "/smart-brain/index.html"
+  },
+  {
+    "revision": "33811c6c2a029dbb12bc",
+    "url": "/smart-brain/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "1e1378740053846b8121",
+    "url": "/smart-brain/static/css/main.672e5bab.chunk.css"
+  },
+  {
+    "revision": "33811c6c2a029dbb12bc",
+    "url": "/smart-brain/static/js/2.c1680f76.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/smart-brain/static/js/2.c1680f76.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1e1378740053846b8121",
+    "url": "/smart-brain/static/js/main.5d963ce4.chunk.js"
+  },
+  {
+    "revision": "257db6b14da30691b4a8",
+    "url": "/smart-brain/static/js/runtime-main.1bac4abf.js"
+  }
+]);
